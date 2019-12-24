8c18489eef88396903889246b3fbeb595bf4b5f8
MeltAlone <1532998154@qq.com>
2019-12-24 21:28:48 +0800

    4-5.React动画练习

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import StudentContainer from "./components/StudentContainer"
+import React, { useState } from 'react'
+import FadeTransition from "./components/common/FadeTransition"
+import { TransitionGroup } from 'react-transition-group'
+import uuid from 'uuid'
 
 export default function App() {
+    const [tasks, setTasks] = useState([
+        {id: uuid(), name: "task1"},
+        {id: uuid(), name: "task2"},
+        {id: uuid(), name: "task3"}
+    ])
     return (
         <div>
-            <StudentContainer/>
+            <TransitionGroup component="ul">
+                {tasks.map(it => (
+                    <FadeTransition 
+                        appear 
+                        timeout={3000}
+                        key={it.id}>
+                        <li>
+                            {it.name}
+                            <button onClick={() => {
+                                setTasks(
+                                    tasks.filter(i => it.id !== i.id)
+                                )
+                            }}>删除</button>
+                        </li>
+                    </FadeTransition>
+                ))}
+            </TransitionGroup>
+            <button onClick={() => {
+                let Newtask = window.prompt("请输入任务名称");
+                setTasks([...tasks, {id: uuid(), name: Newtask}])
+            }}>添加</button>
         </div>
     )
 }
